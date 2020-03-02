b0f14759261184881a51679579254aed8ba9a8be
Tobi Schweiger <tsgp3@mst.edu>
2020-03-02 11:29:42 -0600

    Revert "Basic changes"

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import { Elm } from './App/Main.elm';
+import { Elm } from './Main.elm';
 import registerServiceWorker from './registerServiceWorker';
 
-Elm.App.Main.init({
+Elm.Main.init({
   node: document.getElementById('root')
 });
 
