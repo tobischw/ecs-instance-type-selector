7136b1c3a4aa7fd42efa09983a02a670659c4a3c
Tobias Schweiger <tobischw@tobi.local>
2020-03-03 13:32:38 -0600

    Add flex

--- src/App/Configuration.elm
+++ src/App/Configuration.elm
@@ -5,6 +5,9 @@ import Bootstrap.Grid as Grid
 import Bootstrap.Grid.Col as Col
 import Bootstrap.Button as Button
 import Bootstrap.ListGroup as ListGroup
+import Bootstrap.Utilities.Flex as Flex
+import Bootstrap.Utilities.Size as Size
+import Bootstrap.Utilities.Spacing as Spacing
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
@@ -31,22 +34,3 @@ view =
                 ]
             ]
         ]
-
-
-
-{-
-   view : Grid.Column msg
-   view =
-       Grid.col [ Col.md3, Col.attrs [ class "p-0 bg-light sidebar" ] ]
-           [ div [ class "px-3", class "pt-1" ]
-               [ Util.viewColumnTitle "Configuration"
-               , ListGroup.ul
-                   [ ListGroup.li [] [ Util.icon "weather-cloudy", text "Service A" ]
-                   , ListGroup.li [ ListGroup.attrs [ class "pl-4" ] ] [ Util.icon "clipboard", text "Task A" ]
-                   , ListGroup.li [ ListGroup.active, ListGroup.attrs [ class "pl-5" ] ] [ Util.icon "archive", text "Container 1a" ]
-                   , ListGroup.li [ ListGroup.attrs [ class "pl-5" ] ] [ Util.icon "archive", text "Container 2a" ]
-                   , ListGroup.li [] [ Util.icon "weather-cloudy", text "Service B" ]
-                   ]
-               ]
-           ]
--}
