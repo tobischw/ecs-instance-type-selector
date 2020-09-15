cfb6aa56dcbd5a4b1c180d29a0157fe1b3f55794
PaxPax <ehaysle@siue.edu>
2020-09-15 14:28:11 -0500

    built app files to deploy

---
+++ build/precache-manifest.719fc4ffb6fec6e0ec4e051fc61c2b6d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58e98c42ac1bc0eee3c9ebf547b428a8",
+    "url": "./aws-sdk.min.js"
+  },
+  {
+    "revision": "b0f36ae9d50451a9913ef572c5de37ac",
+    "url": "./dashboard.css"
+  },
+  {
+    "revision": "e12665249d21ff8d13772b70ec2f43ff",
+    "url": "./ec2.svg"
+  },
+  {
+    "revision": "6639df213d37693c848e7b4e8c0bca63",
+    "url": "./favicon.ico"
+  },
+  {
+    "revision": "beac4ffd6997fc7d760fa03db920c8af",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4f0ec8d02e294c606ef740968de21695",
+    "url": "./logo.png"
+  },
+  {
+    "revision": "d533eec35a2991535df840cbf6f84791",
+    "url": "./manifest.json"
+  },
+  {
+    "revision": "d9a7d7ae94244cff58cf",
+    "url": "./static/js/main.d9a7d7ae.chunk.js"
+  },
+  {
+    "revision": "d5752d2fdce3c7e150f5",
+    "url": "./static/js/runtime~main.d5752d2f.js"
+  },
+  {
+    "revision": "b9063112a1dd4b93007d",
+    "url": "./static/js/vendors~main.b9063112.chunk.js"
+  }
+]);
